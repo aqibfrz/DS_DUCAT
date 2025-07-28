469250b9e01a827bcdb87cb98648887354fce45d
Aqib Firoz <aquibfiroz15@gmail.com>
2025-07-28 12:44:50 +0530

    add new files

---
+++ check/file.py
@@ -0,0 +1,12 @@
+def add(num1, num2):
+    return num1+num2
+
+
+if __name__=='__main__':
+    # name=input('Enter your name')
+    # print(add(2,3))
+    # print(name)
+
+    print('this')
+    print(__name__)
+    print(add(33,2))
---
+++ check/file2.py
@@ -0,0 +1,5 @@
+import file
+a=file.add(3,4)
+print(a)
+
+print(__name__)
